210204aec225ec57fc20aab102907fda08962fe9
alesha <alexey.proskuryakov@gmail.com>
2015-11-17 17:33:09 +0300

    see changed params

--- src/msngr/taxi/TaxiWatching.go
+++ src/msngr/taxi/TaxiWatching.go
@@ -110,12 +110,94 @@ type TaxiContext struct {
 	Cars     *CarsCache
 	Notifier *n.Notifier
 }
+func process_state_change(taxiContext *TaxiContext, botContext *s.BotContext, api_order Order, db_order *d.OrderWrapper, previous_states map[int64]int) {
+	log.Printf("WATCH state of: %+v is updated (api: %v != db: %v)", api_order.ID, api_order.State, db_order.OrderState)
+	order_data := api_order.ToOrderData()
+	err := taxiContext.DataBase.Orders.SetState(api_order.ID, botContext.Name, api_order.State, &order_data)
+	if err != nil {
+		log.Printf("WATCH for order %+v can not update status %+v", api_order.ID, api_order.State)
+		return
+	}
+	db_order.OrderState = api_order.State
+	db_order.OrderId = api_order.ID
+
+	//если заказ отменил не пользователь
+	if api_order.State == ORDER_CANCELED {
+		log.Printf("WATCH NOTIFYING THAT ORDER IS CANCELED")
+		taxiContext.Notifier.Notify(s.OutPkg{
+			To:db_order.Whom,
+			Message: &s.OutMessage{
+				ID: u.GenId(),
+				Type: "chat",
+				Body: "Ваш заказ отменен!",
+				Commands: botContext.Commands["commands_at_not_created_order"],
+			},
+		})
+		previous_states[api_order.ID] = api_order.State
+		return
+	}
+	//
+	if car_info := taxiContext.Cars.GetCarInfo(api_order.IDCar); car_info != nil {
+		var notification_data *s.OutPkg
+		arrival_time := api_order.TimeArrival
+		if arrival_time == nil {
+			arrival_time = api_order.TimeDelivery
+			if arrival_time == nil {
+				arrival_time_ := time.Now().Add(5 * time.Minute)
+				arrival_time = &arrival_time_
+			}
+		}
+
+		prev_state, ok := previous_states[api_order.ID]
+		if ok {
+			notification_data = FormNotification(db_order, prev_state, *car_info, *arrival_time)
+		} else {
+			notification_data = FormNotification(db_order, -1, *car_info, *arrival_time)
+		}
+		if notification_data != nil {
+			notification_data.Message.Commands = form_commands_for_current_order(db_order, botContext.Commands)
+			taxiContext.Notifier.Notify(*notification_data)
+			log.Printf("WATCH sended for order [%+v]:\n %#v \n and notify that: \n %#v", db_order.OrderId, notification_data.Message.Commands, *notification_data)
+		}
+	}
+}
+
+func process_car_state(taxiContext *TaxiContext, botContext *s.BotContext, api_order Order, db_order *d.OrderWrapper) {
+	var result s.OutPkg
+
+	result.To = db_order.Whom
+	car_info := taxiContext.Cars.GetCarInfo(api_order.IDCar)
+	if car_info == nil {
+		log.Printf("ALERT! CAR CHANGED TO NOT RECOGNIZED ID")
+		return
+	}
+	result.Message.Body = fmt.Sprintf("Ваша машина изменилась на %v", car_info)
+	taxiContext.Notifier.Notify(result)
+}
+
+const (
+	CHANGE_STATE = "state"
+	CHANGE_CAR = "car"
+
+)
+
+func get_changes(api_order Order, db_order *d.OrderWrapper) []string {
+	var buff []string
+	if result, ok := db_order.OrderData.Get("IDCar").(int64); ok && result != api_order.IDCar {
+		buff = append(buff, CHANGE_CAR)
+	}
+	if api_order.State != db_order.OrderState {
+		buff = append(buff, CHANGE_STATE)
+	}
+	return buff
+
+}
 
 func TaxiOrderWatch(taxiContext *TaxiContext, botContext *s.BotContext) {
 	previous_states := map[int64]int{}
 	for {
 		api_orders := taxiContext.API.Orders()
-//		log.Printf("result of orders request: %v", len(api_orders))
+		//		log.Printf("result of orders request: %v", len(api_orders))
 		for _, api_order := range api_orders {
 			db_order, err := taxiContext.DataBase.Orders.GetById(api_order.ID, botContext.Name)
 			if err != nil {
@@ -126,57 +208,16 @@ func TaxiOrderWatch(taxiContext *TaxiContext, botContext *s.BotContext) {
 				log.Printf("WATCH order [%+v] is not present in system :(\n", api_order)
 				continue
 			}
-			if api_order.State != db_order.OrderState {
-				log.Printf("WATCH state of: %+v is updated (api: %v != db: %v)", api_order.ID, api_order.State, db_order.OrderState)
-				order_data := api_order.ToOrderData()
-				err := taxiContext.DataBase.Orders.SetState(api_order.ID, botContext.Name, api_order.State, &order_data)
-				if err != nil {
-					log.Printf("WATCH for order %+v can not update status %+v", api_order.ID, api_order.State)
-					continue
-				}
-				db_order.OrderState = api_order.State
-				db_order.OrderId = api_order.ID
 
-				//если заказ отменил не пользователь
-				if api_order.State == ORDER_CANCELED {
-					log.Printf("WATCH NOTIFYING THAT ORDER IS CANCELED")
-					taxiContext.Notifier.Notify(s.OutPkg{
-						To:db_order.Whom,
-						Message: &s.OutMessage{
-							ID: u.GenId(),
-							Type: "chat",
-							Body: "Ваш заказ отменен!",
-							Commands: botContext.Commands["commands_at_not_created_order"],
-						},
-					})
-					previous_states[api_order.ID] = api_order.State
-					continue
-				}
-
-				if car_info := taxiContext.Cars.GetCarInfo(api_order.IDCar); car_info != nil {
-					var notification_data *s.OutPkg
-					arrival_time := api_order.TimeArrival
-					if arrival_time == nil {
-						arrival_time = api_order.TimeDelivery
-						if arrival_time == nil {
-							arrival_time_ := time.Now().Add(5 * time.Minute)
-							arrival_time = &arrival_time_
-						}
-					}
-
-					prev_state, ok := previous_states[api_order.ID]
-					if ok {
-						notification_data = FormNotification(db_order, prev_state, *car_info, *arrival_time)
-					} else {
-						notification_data = FormNotification(db_order, -1, *car_info, *arrival_time)
-					}
-					if notification_data != nil {
-						notification_data.Message.Commands = form_commands_for_current_order(db_order, botContext.Commands)
-						taxiContext.Notifier.Notify(*notification_data)
-						log.Printf("WATCH sended for order [%+v]:\n %#v \n and notify that: \n %#v", db_order.OrderId, notification_data.Message.Commands, *notification_data)
-					}
+			for _, el := range get_changes(api_order, db_order) {
+				switch el {
+				case CHANGE_STATE:
+					process_state_change(taxiContext, botContext, api_order, db_order, previous_states)
+				case CHANGE_CAR:
+					process_car_state(taxiContext, botContext, api_order, db_order)
 				}
 			}
+
 			previous_states[api_order.ID] = api_order.State
 		}
 		time.Sleep(1 * time.Second)
